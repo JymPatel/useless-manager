ad5709b1723fdeeeae366a3050e18b05b26a61a0
Jym Patel <jympatel@yahoo.com>
2022-07-09 23:59:27 +0530

    trying to create better version of manager
    new manager.py with
    - better functions
    - use of OOP for creating transactions made
    & more

--- program/manager.py
+++ program/manager.py
@@ -1,72 +1,47 @@
-#!/usr/bin/python3
-# by @JymPatel
-# this code is licensed under GPL v3 License
-print("this code is licensed under GPL v3 License\n")
-manager_version = "0.1.0"
-
 import sys
 from termcolor import colored
-import scripts.functions as fn
-import scripts.load_save as ldsv
 
 
-arg = sys.argv[1:]
-# get data & profiles
-try:
-    [balance_sheet, profiles, settings] = ldsv.load_data()
-except ValueError:
-    print("would you like to reset data")
+
+class Transaction:
+    ...
 
 
-# check number of arguements & get task to do
-if (len(arg) < 1):
-    print("insufficient arguements, opening help file ...")
-    fn.help()
-    exit()
-task = arg[0].lower()
+class Person:
+    ...
 
 
-# help
-if (task == "-h") or (task == "--help"):
-    print("opening docs/help.txt ...")
-    fn.help()
-
-# reset data
-elif (task == "-rb") or (task == "--reset-balance"):
-    balance_sheet = fn.reset_data()
-
-# reset profiles
-elif (task == "-rp") or (task == "--reset-profiles"):
-    fn.reset_profiles()
-
-# reset settings
-elif (task == "-rs") or (task == "--reset-settings"):
-    fn.reset_settings()
-
-# === adding amount ===
-elif (task == "-ab") or (task == "--add-balance"):
-    username = arg[1]
-    if (len(arg) < 3): # check for sufficient arguements
-        print(colored("insufficient arguements", "red"), "for -a command need", colored("-a *user *amount", "red"))
-        fn.help()
-    else:
-        # == updating data ==
-        if username in balance_sheet:
-            balance_sheet[username] += int(arg[2])
-        else:
-            balance_sheet[username] = int(arg[2])
-        new_amnt = balance_sheet[username]
-        print(username, ":", colored(new_amnt, "red" if new_amnt > 0 else "green"))
-        # == send email ==
-        if (username in profiles) and (profiles[username][2] == True):
-            if settings["default_email"] == None:
-                sender_email = input("senders email: ")
-            else:
-                sender_email = settings["default_email"]
-            fn.send_mail(balance_sheet[username], profiles[username][0], profiles[username][1], sender_email, input("password : "))
-        else:
-            print("no profile found for", username, "would you like to create one and send mail")
+def main():
+    task = get_task()
+    
+    if task == "version":
+        print(f"manager.py\ncurrent version {current_version()[0]}.{current_version()[1]}")
 
 
-# save data
-ldsv.save_data(balance_sheet, profiles, settings)
+def get_task():
+    try: 
+        task =  sys.argv[1]
+
+        if task in ["-h", "--help", "help"]:
+            return "help"
+        if task in ["-v", "--version", "version"]:
+            return "version"
+
+        else: # else return error 102 in RED
+            generated_error = "ERROR 102\nCould not find task provided\nTry manager.py --help"
+            sys.exit(colored(generated_error, "red"))
+
+    except IndexError: # no arguements given
+        generated_error = "ERROR 101\nInsufficient argurments\nTry manager.py --help"
+        sys.exit(colored(generated_error, "red"))
+
+
+
+
+def current_version():
+    major = 1
+    minor = 1
+    return [major, minor]
+
+if __name__ == __name__:
+    main()
